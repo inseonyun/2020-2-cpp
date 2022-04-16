4c1c05427ff64623e9949cb5f2ff5b39625e4504
inseonyun <yuninseon@naver.com>
2022-04-16 16:39:22 +0900

    Add a week9 cpp files

---
+++ week9/quiz01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+/*
+int getExp(int x, int y) {
+	int value = 1;
+	for (int i = 0; i < y; i++) 
+		value = value * x;
+	return value;
+}
+
+int main2() {
+	int v = getExp(2, 3);
+	cout << "2의 3승은 " << v << "입니다." << endl;
+	
+	int e = getExp(2, -3);
+	cout << "2의 -3승은 " << e << "입니다." << endl;
+	return 0;
+}
+*/
---
+++ week9/quiz03.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+/*
+bool getExp(int x, int y, int &ret) {
+	if (x <= 0 || y <= 0) {
+		return false;
+	}
+	int value = 1;
+	for (int i = 0; i < y; i++)
+		value = value * x;
+	ret = value;
+	return true;
+}
+
+int main() {
+	int v = 0;
+	if (getExp(2, 3, v))
+		cout << "2의 3승은 " << v << "입니다." << endl;
+	else
+		cout << "오류. 2의 3승은 " << "계산할 수 없습니다." << endl;
+
+	int e = 0;
+	if (getExp(2, -3, e))
+		cout << "2의 3승은 " << e << "입니다." << endl;
+	else
+		cout << "오류. 2의 3승은 " << "계산할 수 없습니다." << endl;
+
+}
+*/
